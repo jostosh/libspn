b25ef28029d24ddd8cf1c7c603170a30823ee2e1
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-04-23 12:36:10 +0200

    initial attempt on DynamicGaussLeafs

--- libspn/graph/distribution.py
+++ libspn/graph/distribution.py
@@ -6,7 +6,7 @@
 # ------------------------------------------------------------------------
 import tensorflow as tf
 from libspn.graph.scope import Scope
-from libspn.graph.node import VarNode
+from libspn.graph.node import VarNode, DynamicVarNode
 from libspn import conf
 from libspn import utils
 import numpy as np
@@ -124,24 +124,24 @@ class GaussianLeaf(VarNode):
     def _compute_out_size(self):
         return self._num_vars * self._num_components
 
-    def _tile_feed(self):
-        return tf.tile(tf.expand_dims(self._feed, -1), [1, 1, self._num_components])
+    def _tile_feed(self, feed):
+        return tf.tile(tf.expand_dims(feed, -1), [1, 1, self._num_components])
 
-    def _tile_evidence(self):
-        return tf.tile(self.evidence, [1, self._num_components])
+    def _tile_evidence(self, evidence):
+        return tf.tile(evidence, [1, self._num_components])
 
     def _compute_value(self):
         # self._assert_built()
         dist = tfd.Normal(loc=self._mean_variable, scale=tf.sqrt(self._variance_variable))
         evidence_probs = tf.reshape(
-            dist.prob(self._tile_feed()), (-1, self._compute_out_size()))
+            dist.prob(self._tile_feed(self._feed)), (-1, self._compute_out_size()))
         return tf.where(self._tile_evidence(), evidence_probs, tf.ones_like(evidence_probs))
 
     def _compute_log_value(self):
         # self._assert_built()
         dist = tfd.Normal(loc=self._mean_variable, scale=tf.sqrt(self._variance_variable))
         evidence_probs = tf.reshape(
-            dist.log_prob(self._tile_feed()), (-1, self._compute_out_size()))
+            dist.log_prob(self._tile_feed(self._feed)), (-1, self._compute_out_size()))
         return tf.where(self._tile_evidence(), evidence_probs, tf.zeros_like(evidence_probs))
 
     def _compute_scope(self):
@@ -161,6 +161,179 @@ class GaussianLeaf(VarNode):
         sum_data = tf.reduce_sum(counts_reshaped * tiled_feed, axis=0)
         sum_data_squared = tf.reduce_sum(counts_reshaped * tf.square(tiled_feed), axis=0)
         return {'accum': accum, "sum_data": sum_data, "sum_data_squared": sum_data_squared}
+
+    def assign(self, accum, sum_data, sum_data_squared):
+        total_counts = self._total_count_variable + accum
+        mean = (self._total_count_variable * self._mean_variable + sum_data) / (total_counts + 1e-10)
+        # dx = x - mean
+        # dx.dot(dx) == \sum x^2 - 2 * mean * \sum x + n * mean^2
+        variance = (self._total_count_variable * self._variance_variable +
+                    sum_data_squared - 2 * self._mean_variable * sum_data +
+                    accum * tf.square(self._mean_variable)) / \
+                   (total_counts + 1e-10) - tf.square(mean - self._mean_variable)
+
+        return (
+            tf.assign(self._total_count_variable, total_counts),
+            tf.assign(self._variance_variable, variance),
+            tf.assign(self._mean_variable, mean)
+        )
+
+
+class DynamicGaussianLeaf(DynamicVarNode, GaussianLeaf):
+
+    def __init__(self, max_steps, feed=None, num_vars=1, num_components=2, name="GaussianLeaf",
+                 data=None, learn_scale=True, total_counts_init=1, learn_dist_params=False,
+                 time_major=False):
+        self._mean_variable = None
+        self._variance_variable = None
+        self._num_vars = num_vars
+        self._num_components = num_components
+        self._mean_init = tf.zeros((num_vars, num_components), dtype=conf.dtype)
+        self._variance_init = tf.ones((num_vars, num_components), dtype=conf.dtype)
+        self._learn_dist_params = learn_dist_params
+        if data is not None:
+            self.learn_from_data(data, learn_scale=learn_scale)
+        super(DynamicVarNode).__init__(
+            max_steps=max_steps, feed=feed, name=name, time_major=time_major)
+
+        var_shape = (num_vars, num_components)
+        self._total_count_variable = self._total_accumulates(total_counts_init, var_shape)
+    #
+    # def initialize(self):
+    #     return (self._mean_variable.initializer, self._variance_variable.initializer,
+    #             self._total_count_variable.initializer)
+    #
+    # @property
+    # def variables(self):
+    #     return self._mean_variable, self._variance_variable
+    #
+    # @property
+    # def mean_variable(self):
+    #     return self._mean_variable
+    #
+    # @property
+    # def variance_variable(self):
+    #     return self._variance_variable
+    #
+    # @property
+    # def evidence(self):
+    #     return self._evidence_indicator
+    #
+    # @property
+    # def learn_distribution_parameters(self):
+    #     return self._learn_dist_params
+
+    def _create_placeholder(self):
+        return tf.placeholder(conf.dtype, [self._max_steps, None, self._num_vars])
+
+    def _create_evidence_indicator(self):
+        return tf.placeholder_with_default(
+            tf.cast(tf.ones_like(self._placeholder), tf.bool),
+            shape=[self._max_steps, None, self._num_vars])
+    #
+    # def _create(self):
+    #     super()._create()
+    #     self._mean_variable = tf.Variable(
+    #         self._mean_init, dtype=conf.dtype, collections=['spn_distribution_parameters'])
+    #     self._variance_variable = tf.Variable(
+    #         self._variance_init, dtype=conf.dtype, collections=['spn_distribution_parameters'])
+    #     self._evidence_indicator = self._create_evidence_indicator()
+    #
+    # def learn_from_data(self, data, learn_scale=True):
+    #     """Learns the distribution parameters from data
+    #     Params:
+    #         data: numpy.ndarray of shape [batch, num_vars]
+    #     """
+    #     if len(data.shape) != 2 or data.shape[1] != self._num_vars:
+    #         raise ValueError("Data should be of rank 2 and contain equally many variables as this "
+    #                          "GaussianQuantile node.")
+    #
+    #     values_per_quantile = self._values_per_quantile(data)
+    #
+    #     self._mean_init = np.stack(
+    #         [np.mean(values, axis=0) for values in values_per_quantile], axis=-1)
+    #     if learn_scale:
+    #         self._variance_init = np.stack(
+    #             [np.var(values, axis=0) for values in values_per_quantile], axis=-1)
+    #     else:
+    #         self._variance_init = np.ones_like(self._mean_init)
+
+    def serialize(self):
+        data = super().serialize()
+        data['num_vars'] = self._num_vars
+        data['num_components'] = self._num_components
+        data['mean_init'] = self._mean_init
+        data['variance_init'] = self._variance_init
+        return data
+
+    def deserialize(self, data):
+        self._num_vars = data['num_vars']
+        self._num_components = data['num_components']
+        self._mean_init = data['mean_init']
+        self._variance_init = data['variance_init']
+        super().deserialize(data)
+
+    # def _total_accumulates(self, init_val, shape):
+    #     init = utils.broadcast_value(init_val, shape, dtype=conf.dtype)
+    #     return tf.Variable(init, name=self.name + "TotalCounts", trainable=False)
+    #
+    # def _values_per_quantile(self, data):
+    #     batch_size = data.shape[0]
+    #     quantile_sections = np.arange(
+    #         batch_size // self._num_components, batch_size, batch_size // self._num_components)
+    #     sorted_features = np.sort(data, axis=0).astype(tf.DType(conf.dtype).as_numpy_dtype())
+    #     values_per_quantile = np.split(
+    #         sorted_features, indices_or_sections=quantile_sections, axis=0)
+    #     return values_per_quantile
+
+    # def _compute_out_size(self):
+    #     return self._num_vars * self._num_components
+    #
+    # def _tile_feed(self):
+    #     return tf.tile(tf.expand_dims(self._feed, -1), [1, 1, self._num_components])
+    #
+    # def _tile_evidence(self):
+    #     return tf.tile(self.evidence, [1, self._num_components])
+
+    def _compute_value(self, time):
+        # self._assert_built()
+        feed_array = self._unstacked_tensor(self._feed)
+        evidence_array = self._unstacked_tensor(self.evidence)
+        dist = tfd.Normal(loc=self._mean_variable, scale=tf.sqrt(self._variance_variable))
+        evidence_probs = tf.reshape(
+            dist.prob(self._tile_feed(feed_array.read(time))), (-1, self._compute_out_size()))
+        return tf.where(
+            self._tile_evidence(evidence_array.read(time)),
+            evidence_probs, tf.ones_like(evidence_probs))
+
+    def _unstacked_tensor(self, tensor):
+        return tf.TensorArray(dtype=conf.dtype, size=self._max_steps).unstack(value=tensor)
+
+    def _compute_log_value(self, time):
+        # self._assert_built()
+        feed_array = self._unstacked_tensor(self._feed)
+        evidence_array = self._unstacked_tensor(self.evidence)
+        dist = tfd.Normal(loc=self._mean_variable, scale=tf.sqrt(self._variance_variable))
+        evidence_probs = tf.reshape(
+            dist.log_prob(self._tile_feed(feed_array.read(time))), (-1, self._compute_out_size()))
+        return tf.where(
+            self._tile_evidence(evidence_array.read(time)),
+            evidence_probs, tf.zeros_like(evidence_probs))
+    #
+    # def _compute_mpe_state(self, counts):
+    #     # self._assert_built()
+    #     counts_reshaped = tf.reshape(counts, (-1, self._num_vars, self._num_components))
+    #     indices = tf.argmax(counts_reshaped, axis=-1) + tf.expand_dims(
+    #         tf.range(self._num_vars, dtype=tf.int64) * self._num_components, axis=0)
+    #     return tf.gather(tf.reshape(self._mean_variable, (-1,)), indices=indices, axis=0)
+    #
+    # def _compute_hard_em_update(self, counts):
+    #     counts_reshaped = tf.reshape(counts, (-1, self._num_vars, self._num_components))
+    #     accum = tf.reduce_sum(counts_reshaped, axis=0)
+    #     tiled_feed = self._tile_feed()
+    #     sum_data = tf.reduce_sum(counts_reshaped * tiled_feed, axis=0)
+    #     sum_data_squared = tf.reduce_sum(counts_reshaped * tf.square(tiled_feed), axis=0)
+    #     return {'accum': accum, "sum_data": sum_data, "sum_data_squared": sum_data_squared}
 
     def assign(self, accum, sum_data, sum_data_squared):
         total_counts = self._total_count_variable + accum
