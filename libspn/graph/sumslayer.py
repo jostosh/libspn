a81306077c66f74741bf411e122b550e8516683a
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-04-25 15:44:40 +0200

    small fix in sumslayer

--- libspn/graph/sumslayer.py
+++ libspn/graph/sumslayer.py
@@ -458,7 +458,9 @@ class SumsLayer(OpNode):
         return reduction_fn(reducible_values)
 
     def _apply_weighting(self, cwise_op, reducible_values, weight_tensor):
+        print(reducible_values.shape)
         reducible_values = cwise_op(reducible_values, weight_tensor)
+        print(reducible_values.shape)
         return reducible_values
 
     def _apply_IVs(self, cwise_op, ivs_tensor, reducible_values):
@@ -678,9 +680,13 @@ class SumsLayer(OpNode):
                               add_random=None, use_unweighted=False, with_ivs=True):
         # Get weighted, IV selected values
         weighted = not use_unweighted or any(v.node.is_var for v in self._values)
+        print("UNWEIGHTED", use_unweighted)
         values_weighted = self._compute_value_common(
             tf.add, lambda x: x, weight_value, ivs_value, *value_values,
             weighted=weighted, pad_elem=-float('inf'), with_ivs=with_ivs)
+        if values_weighted.shape[1].value == 1 and self._num_sums > 1:
+            # This might be necessary when compute_value_common assumes broadcasting
+            values_weighted = tf.tile(values_weighted, (1, self._num_sums, 1))
 
         # WARN ADDING RANDOM NUMBERS
         if add_random is not None:
